a2504e0909e58800a90a58feb8536851b5666d13
mateusz.pusiewicz <mateusz.pusiewicz@ing.com>
2023-07-15 20:02:07 +0200

    Populate database with todo data

--- src/main/java/com/mattpus/springboot/todoApp/todo/Todo.java
+++ src/main/java/com/mattpus/springboot/todoApp/todo/Todo.java
@@ -1,12 +1,14 @@
 package com.mattpus.springboot.todoApp.todo;
-
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDate;
 
 //Database (MySQL)
 //Static List of todos => Database (H2, MySQL)
-
+@Entity
 public class Todo {
 
     public Todo(int id, String username, String description, LocalDate targetDate, boolean done) {
@@ -18,13 +20,20 @@ public class Todo {
         this.done = done;
     }
 
+    @Id
+    @GeneratedValue
     private int id;
+
     private String username;
 
     @Size(min=8, message="Enter at least 8 characters")
     private String description;
     private LocalDate targetDate;
     private boolean done;
+
+    public Todo() {
+
+    }
 
     public int getId() {
         return id;
