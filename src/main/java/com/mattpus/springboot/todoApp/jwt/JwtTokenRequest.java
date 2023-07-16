5e86bb1ae6b28c75cdf8996a2e0c5afbab8f0092
mateusz.pusiewicz <mateusz.pusiewicz@ing.com>
2023-07-16 20:23:39 +0200

    Implement java web tokens to improve security

---
+++ src/main/java/com/mattpus/springboot/todoApp/jwt/JwtTokenRequest.java
@@ -0,0 +1,3 @@
+package com.mattpus.springboot.todoApp.jwt;
+
+public record JwtTokenRequest(String username, String password) {}
---
+++ src/main/java/com/mattpus/springboot/todoApp/jwt/JwtTokenResponse.java
@@ -0,0 +1,3 @@
+package com.mattpus.springboot.todoApp.jwt;
+
+public record JwtTokenResponse(String token) {}
